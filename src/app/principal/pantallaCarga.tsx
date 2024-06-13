a25931d814faf546fb5706f2896324fff1455291
Mateo <maldonado12net@gmail.com>
2024-06-13 14:50:31 -0300

    arreglado

--- src/app/principal/pantallaCarga.tsx
+++ src/app/principal/pantallaCarga.tsx
@@ -5,32 +5,38 @@ interface pantallaCargaProps {
     cargandoPrincipal: boolean
     cargandoChats: boolean
     cargandoImagenes: boolean
-  }
-const PantallaCarga: React.FC<pantallaCargaProps> = ({cargandoPrincipal, cargandoChats, cargandoImagenes}) => {
+}
+const PantallaCarga: React.FC<pantallaCargaProps> = ({ cargandoPrincipal, cargandoChats, cargandoImagenes }) => {
 
-    const [puntos, setPuntos] = useState(".");
+    const [puntos, setPuntos] = useState(".  ");
 
     useEffect(() => {
-        if(cargandoPrincipal || cargandoChats || cargandoImagenes){
-        const interval = setInterval(() => {
-            if(puntos.length < 3){
-                setPuntos(puntos + ".");
-            }
-            else{
-                setPuntos(".");
-            }
-        }, 400);
-        return () => clearInterval(interval);
-    }
+        if (cargandoPrincipal || cargandoChats || cargandoImagenes) {
+            const interval = setInterval(() => {
+                switch (puntos) {
+                    case ".  ":
+                        setPuntos(".. ");
+                        break;
+                    case ".. ":
+                        setPuntos("...");
+                        break;
+                    case "...":
+                        setPuntos(".  ");
+                        break;
+                }
+            }, 400);
+            return () => clearInterval(interval);
+        }
     }, [puntos]);
 
-    if(cargandoPrincipal || cargandoChats || cargandoImagenes){
-        return(
+    if (cargandoPrincipal || cargandoChats || cargandoImagenes) {
+        return (
             <div className="flex flex-col items-center justify-center fixed inset-0 w-full h-full z-50 opacity-80" style={{
                 backgroundImage: `url(${backgroundImage.src})`,
                 backgroundSize: 'cover',
-                backgroundPosition: 'center'}}>
-                
+                backgroundPosition: 'center'
+            }}>
+
                 <h1 className="border-solid mb-4 text-8xl font-stoothgart text-black-500">Cargando{puntos}</h1>
             </div>
         )
